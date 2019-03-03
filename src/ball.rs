c89ac102a0f65742e4660b2eaeb81aa547935c31
Ken Cruz <ken@kencruz.ca>
2019-03-03 12:34:01 -0500

    basic ball horizontal bounce mechanics

--- src/ball.rs
+++ src/ball.rs
@@ -21,13 +21,17 @@ impl Ball {
         Ball {
             shape: Rect::from_center(Point::new(400, 300), 10, 10),
             pos: Coords{x: 400, y: 300},
-            vel: Velocity{ x: -2, y: 0},
+            vel: Velocity{ x: 5, y: 0},
         }
     }
 
     pub fn update(&mut self) {
-        if self.shape.left() < 0 {
-            self.shape.set_x(400);
+        if self.shape.center().x() < 0 {
+            self.vel.x *= -1;
+            self.shape.set_x(1);
+        } else if self.shape.center().x() > 800 {
+            self.vel.x *= -1;
+            self.shape.set_x(795);
         } else {
             self.shape.set_x(self.shape.left() + self.vel.x);
             self.shape.set_y(self.shape.top() + self.vel.y);
